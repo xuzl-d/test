1ea1181adf53bfa7c9a24f6bd771bc061d3a3e1c
xuzl-d <69352418+xuzl-d@users.noreply.github.com>
2023-09-20 20:18:06 +0800

    delete makefile

--- cmkaeDemo2/build/src/CalcProject/CMakeFiles/calc.dir/add.cpp.o.d
+++
@@ -1,2 +0,0 @@
-src/CalcProject/CMakeFiles/calc.dir/add.cpp.o: \
-  /Users/hoje/Documents/Code/cmkaeDemo2/src/CalcProject/add.cpp
--- cmkaeDemo2/build/src/CalcProject/CMakeFiles/calc.dir/sub.cpp.o.d
+++
@@ -1,2 +0,0 @@
-src/CalcProject/CMakeFiles/calc.dir/sub.cpp.o: \
-  /Users/hoje/Documents/Code/cmkaeDemo2/src/CalcProject/sub.cpp
